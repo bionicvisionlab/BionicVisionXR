f112698a9b204d6247b97c73118f36baf6e034af
NeuroscienceScripts <justin_kasowski@ucsb.edu>
2021-10-27 10:40:37 -0700

    Updated GUI

--- BionicVisionVR/GUI_Assets/NewGUI_Controller.cs
+++ BionicVisionVR/GUI_Assets/NewGUI_Controller.cs
@@ -6,18 +6,84 @@ using Valve.VR;
 
 public class NewGUI_Controller : MonoBehaviour
 {
+    public Slider device; 
+    public Slider x_pos;
+    public Slider y_pos;
+    public Slider rotation; 
+    public Slider rho;
+    public Slider lambda;
+    public Slider amplitude;
     
-    public Button SPV_active; 
-    
+    public Button SPV_active;
+
+    private int currentGuiTarget;
+    private static int numSliders = 7; 
+    private Slider[] GuiTargets = new Slider[numSliders];
+    private float[] sliderIntervals = new float[numSliders];  
+
+    private double timer;
+    private float interval = .25f; 
+
     // Start is called before the first frame update
     void Start()
     {
+        GuiTargets[0] = device;
+        sliderIntervals[0] = 1f; 
         
+        GuiTargets[1] = x_pos;
+        sliderIntervals[1] = 300f; 
+        
+        GuiTargets[2] = y_pos;
+        sliderIntervals[2] = 300f;
+
+        GuiTargets[3] = rotation; 
+        sliderIntervals[3] = 45f;
+        
+        GuiTargets[4] = rho;
+        sliderIntervals[4] = 1f; 
+        
+        GuiTargets[5] = lambda;
+        sliderIntervals[5] = 1f; 
+        
+        GuiTargets[6] = amplitude;
+        sliderIntervals[6] = 0.1f;
+
+        SelectSlider(); 
+    }
+
+    void SelectSlider()
+    {
+        for (int i=0; i<GuiTargets.Length; i++)
+        {
+            if (currentGuiTarget != i)
+                GuiTargets[i].interactable = false;
+            else
+                GuiTargets[i].interactable = true;
+        }
+    }
+
+    bool TimeElapsed()
+    {
+        if (Time.realtimeSinceStartupAsDouble - timer > interval)
+        {
+            timer = Time.realtimeSinceStartupAsDouble;
+            return true; 
+        }
+        return false; 
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        if (Input.GetKeyDown(KeyCode.DownArrow) && TimeElapsed() && currentGuiTarget != GuiTargets.Length-1)
+            currentGuiTarget++;
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) && TimeElapsed()  && currentGuiTarget != 0)
+            currentGuiTarget--; 
+        
+        SelectSlider();
+        
         if (Input.GetKeyDown(KeyCode.Space) || SteamVR_Actions._default.GrabPinch.GetStateDown(SteamVR_Input_Sources.RightHand))
         {
             if (SPV_active.interactable)
@@ -31,5 +97,12 @@ public class NewGUI_Controller : MonoBehaviour
                 SPV_active.GetComponentInChildren<Text>().text = "SPV Active";
             }
         }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+            GuiTargets[currentGuiTarget].value += sliderIntervals[currentGuiTarget];
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+            GuiTargets[currentGuiTarget].value -= sliderIntervals[currentGuiTarget];
+
     }
 }
